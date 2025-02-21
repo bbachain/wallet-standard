115bc3dc5d2d5b9604c5d38a65e2efe01735aea1
Philip James <131665223+philipdev1@users.noreply.github.com>
2025-02-21 09:10:52 -0800

    Revert "add @bbachain/wallet-standard-bbawallet (#3)" (#4)
    This reverts commit 398540fe1e0244a68f9c2cef0ebfae479373d0cb.

--- packages/wallets/bbawallet/src/initialize.ts
+++
@@ -1,7 +0,0 @@
-import { registerWallet } from './register.js';
-import { BBAWalletWallet } from './wallet.js';
-import type { BBAWallet } from './window.js';
-
-export function initialize(bbaWallet: BBAWallet): void {
-    registerWallet(new BBAWalletWallet(bbaWallet));
-}
